1a80b1da83f963d45a17fff30cf0bc71b5b17a2a
Dae51 <josue.portillo0123@gmail.com>
2026-03-01 09:38:50 -0600

    Comit inicial

---
+++ src/components/ExpenseForm.jsx
@@ -0,0 +1,169 @@
+
+import React, { useContext, useEffect, useState } from 'react'
+
+import { categories } from '../data/categories'
+
+import DatePicker from 'react-date-picker'
+
+import 'react-date-picker/dist/DatePicker.css'
+
+import 'react-calendar/dist/Calendar.css'
+
+import { BudgetDispatchContext, BudgetStateContext } from '../context/BudgetContext'
+
+import ErrorMessage from './ErrorMessage'
+
+export const ExpenseForm = () => {
+
+  const [expense, setExpense] = useState({
+    expenseName: "",
+    amount: "",
+    category: "",
+    date: new Date(),
+  });
+
+  const [error, setError] = useState('')
+  const dispatch = useContext(BudgetDispatchContext)
+  const state=useContext(BudgetStateContext)
+
+  useEffect(()=>{
+    if(state.editingId){
+      const editingExpense=state.expenses.filter(currentExpense=>currentExpense.id===state.editingId)[0]
+      setExpense(editingExpense)
+    }
+  },[state.editingId])
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const isAmountField = ["amount"].includes(name);
+    setExpense({
+      ...expense,
+      [name]: isAmountField ? (value === "" ? "" : Number(value)) : value,
+    })
+  }
+
+   const handleChangeDate = (value) => {
+    setExpense({
+      ...expense,
+      date: value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+
+    if (Object.values(expense).includes('')) {
+      setError('Todos los Campos son Obligatorios')
+      return
+    }
+
+    const totalExpenses = state.expenses.reduce((total, currentExpense) => total + currentExpense.amount, 0)
+    const editingExpense = state.expenses.find(currentExpense => currentExpense.id === state.editingId)
+    const editingAmount = editingExpense ? editingExpense.amount : 0
+    const availableBudget = state.editingId ? state.budget - (totalExpenses - editingAmount) : state.budget - totalExpenses
+
+    if (expense.amount > availableBudget) {
+      setError('El gasto excede el presupuesto disponible')
+      return
+    }
+
+    setError('')
+
+    if (state.editingId) {
+      dispatch({ type: 'update-expense', payload: { expense: { id: state.editingId, ...expense } } })
+    }
+    else {
+      dispatch({ type: 'add-expense', payload: { expense } })
+    }
+
+
+    setExpense({
+      expenseName: "",
+      amount: "",
+      category: "",
+      date: new Date(),
+
+    })
+  }
+
+  return (
+    <form className=" space-y-5" onSubmit={handleSubmit}>
+      <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
+        {state.editingId ? 'Guardar cambios' : 'Nuevo gasto'}
+      </legend>
+
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="expenseName" className="text-xl">
+          Nombre Gasto:
+        </label>
+        <input
+          type="text"
+          id="expenseName"
+          placeholder="Añade el Nombre del gasto"
+          className=" bg-slate-100 p-2"
+          name="expenseName"
+          value={expense.expenseName}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Cantidad:
+        </label>
+        <input
+          type="number"
+          id="amount"
+          placeholder="Añade la Cantidad del gasto: ej. 300"
+          className=" bg-slate-100 p-2"
+          name="amount" // se usa para identificar el campo en el formulario
+          value={expense.amount}
+          onChange={handleChange}
+
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="category" className="text-xl">
+          Categoría:
+        </label>
+        <select
+          id="category"
+          className=" bg-slate-100 p-2"
+          name="category"
+          value={expense.category}
+          onChange={handleChange}
+        >
+          <option> -- Seleccione --</option>
+          {categories.map((category) => (
+            <option
+              key={category.id}
+              value={category.id}
+            >
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Fecha Gasto:
+        </label>
+        <DatePicker
+          className=" bg-slate-100 p-2 border-0" value={expense.date}
+          onChange={handleChangeDate}
+        />
+      </div>
+
+      <input
+        type="submit"
+        className=" bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+        value={state.editingId ? 'Guardar cambios' : 'Registrar gasto'}
+      />
+    </form>
+  )
+}
